28345c6a312d3407524eb2a129510fdd03245a9c
Andishafira <andishafira757@gmail.com>
2024-12-10 21:57:32 +0700

    add

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -3,7 +3,10 @@
 from ultralytics.engine.model import Model
 from ultralytics.models import yolo  # noqa
 from ultralytics.nn.tasks import ClassificationModel, DetectionModel, PoseModel, SegmentationModel
-
+import timm
+from timm.models import swin_transformer
+import torch.nn as nn
+from timm import create_model
 
 class YOLO(Model):
     """
@@ -34,3 +37,14 @@ class YOLO(Model):
                 'trainer': yolo.pose.PoseTrainer,
                 'validator': yolo.pose.PoseValidator,
                 'predictor': yolo.pose.PosePredictor, }, }
+
+# Definisikan SwinTransformer
+class SwinTransformer(nn.Module):
+    def __init__(self, pretrained=True):
+        super(SwinTransformer, self).__init__()
+        # Inisialisasi model dari timm
+        self.model = create_model('swin_base_patch4_window7_224', pretrained=pretrained)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
